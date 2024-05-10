5c50ca224ffbc2f1473b852e7cd617ab45907c4c
Kazantsev Sergey <ksa@MacBook-Air-Kazantsev.local>
2024-05-10 16:15:28 +0500

    s

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -21,4 +21,14 @@ public class tests {
         assertEquals(55, Main._sum(nums));
         assertEquals(3628800, Main._mult(nums));
     }
+
+    @Test
+    public void testFile2() {
+        String path_name = "./test2.txt";
+        List<Integer> nums = readFile(path_name);
+        assertEquals(-5, Main._min(nums));
+        assertEquals(5, Main._max(nums));
+        assertEquals(0, Main._sum(nums));
+        assertEquals(0, Main._mult(nums));
+    }
 }
