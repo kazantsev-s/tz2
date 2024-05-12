e825d8cb65bca7c4461707e0ff4573b233cfd135
Kazantsev Sergey <ksa@MacBook-Air-Kazantsev.local>
2024-05-12 20:22:16 +0300

    final

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -1,5 +1,4 @@
 import org.example.Main;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
@@ -30,5 +29,35 @@ public class tests {
         assertEquals(5, Main._max(nums));
         assertEquals(0, Main._sum(nums));
         assertEquals(0, Main._mult(nums));
+    }
+
+    @Test
+    public void testFile3() {
+        String path_name = "./test3.txt";
+        List<Integer> nums = readFile(path_name);
+        assertEquals(5, Main._min(nums));
+        assertEquals(5, Main._max(nums));
+        assertEquals(25, Main._sum(nums));
+        assertEquals(3125, Main._mult(nums));
+    }
+
+    @Test
+    public void testFile4() {
+        String path_name = "./test4.txt";
+        List<Integer> nums = readFile(path_name);
+        assertEquals(777, Main._min(nums));
+        assertEquals(777, Main._max(nums));
+        assertEquals(777, Main._sum(nums));
+        assertEquals(777, Main._mult(nums));
+    }
+
+    @Test
+    public void testFile5() {
+        String path_name = "./test5.txt";
+        List<Integer> nums = readFile(path_name);
+        assertEquals(100, Main._min(nums));
+        assertEquals(10000, Main._max(nums));
+        assertEquals(11100, Main._sum(nums));
+        assertEquals(1000000000, Main._mult(nums));
     }
 }
